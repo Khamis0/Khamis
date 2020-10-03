d90df35e989a555d23eb31fe9b45fa127cf6a9ca
Khamis08 <39904290+Khamis08@users.noreply.github.com>
2020-10-03 08:53:11 +0300

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -385,7 +385,7 @@ def donate(bot: Bot, update: Update):
 
         if OWNER_ID != 254318997 and DONATION_LINK:
             update.effective_message.reply_text("You can also donate to the person currently running me "
-                                                "[here]({})".format(DONATION_LINK),
+                                                "[here]()".format(DONATION_LINK),
                                                 parse_mode=ParseMode.MARKDOWN)
 
     else:
