6fc3e805ebc1506d50820ace5c6213f5dca9a8fe
Khamis08 <39904290+Khamis08@users.noreply.github.com>
2020-10-12 13:10:23 +0300

    Update welcome.py

--- tg_bot/modules/welcome.py
+++ tg_bot/modules/welcome.py
@@ -87,7 +87,7 @@ def new_member(bot: Bot, update: Update):
         for new_mem in new_members:
             # Give the owner a special welcome
             if new_mem.id == OWNER_ID:
-                update.effective_message.reply_text("Master is in the houseeee, let's get this party started!")
+                update.effective_message.reply_text("Master Karibu Ndani ya Kundi Tuna Tarajia Mengi ya Kheri Kutoka kwako!")
                 continue
 
             # Don't welcome yourself
@@ -154,7 +154,7 @@ def left_member(bot: Bot, update: Update):
 
             # Give the owner a special goodbye
             if left_mem.id == OWNER_ID:
-                update.effective_message.reply_text("RIP Master")
+                update.effective_message.reply_text("Kwaheri 🤝 Master")
                 return
 
             # if media goodbye, use appropriate function for it
