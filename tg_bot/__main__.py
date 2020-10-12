37d488d890347704438c383cf00a4d4b0f6259d2
Khamis08 <39904290+Khamis08@users.noreply.github.com>
2020-10-12 16:11:14 +0300

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -27,13 +27,12 @@ I'm a modular group management bot with a few fun extras! Have a look at the fol
 the things I can help you with.
 
 *Main* commands available:
- - /start: start the bot
- - /help: PM's you this message.
+ - /start: *Start the bot*
+ - /help: *PM's you this message.*
  - /help <module name>: PM's you info about that module.
- - /donate: information about how to donate!
  - /settings:
-   - in PM: will send you your settings for all supported modules.
-   - in a group: will redirect you to pm, with all that chat's settings.
+   - *in PM: will send you your settings for all supported modules.*
+   - *in a group: will redirect you to pm, with all that chat's settings.*
 """
 DONATE_STRING = """
 [
