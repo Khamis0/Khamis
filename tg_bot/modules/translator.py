fa929b6da0c973121dc28000af7c6e13baea9c58
Khamis08 <39904290+Khamis08@users.noreply.github.com>
2020-11-05 22:54:38 +0300

    Update translator.py

--- tg_bot/modules/translator.py
+++ tg_bot/modules/translator.py
@@ -1,8 +1,1 @@
 
-
-
-__help__ = """- /tr (language code) as reply to a long message.
-"""
-__mod_name__ = "Google Translate"
-
-dispatcher.add_handler(DisableAbleCommandHandler("tr", do_translate, pass_args=True))
