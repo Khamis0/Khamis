856b5b0ad999ba0f985a7282d67f7965bc712992
Khamis08 <39904290+Khamis08@users.noreply.github.com>
2020-11-06 18:58:04 +0300

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -19,25 +19,45 @@ from tg_bot.modules.helper_funcs.chat_status import is_user_admin
 from tg_bot.modules.helper_funcs.misc import paginate_modules
 
 PM_START_TEXT = """
-Hi *{}*, my name is *{}*! If you have any questions on how to use me, read /help -
+Hi {}, my name is {}! If you have any questions on how to use me, read /help - and then head to @MarieSupport.
+
+I'm a group manager bot maintained by [this wonderful person](tg://user?id={}). I'm built in python3, using the \
+python-telegram-bot library, and am fully [opensource](https://github.com/paulsonoflars/tgbot) - you can find what makes me tick \
+[here](github.com/aboyhasnooname/tgbot)!
+
+Feel free to submit pull requests on github, or to contact my support group, @MarieSupport, with any bugs, questions \
+or feature requests you might have :)
+I also have a news channel, @MarieNews for announcements on new features, downtime, etc.
+
+You can find the list of available commands with /help.
+
+If you're enjoying using me, and/or would like to help me survive in the wild, hit /donate to help fund/upgrade my VPS!
 """
 
 HELP_STRINGS = """
+Hey there! My name is *{}*.
 I'm a modular group management bot with a few fun extras! Have a look at the following for an idea of some of \
 the things I can help you with.
 
 *Main* commands available:
- - /start: *Start the bot*
- - /help: *PM's you this message.*
- - /help _<module name>: PM's you info about that module._
+ - /start: start the bot
+ - /help: PM's you this message.
+ - /help <module name>: PM's you info about that module.
+ - /donate: information about how to donate!
  - /settings:
-   - *in PM: will send you your settings for all supported modules.*
-   - *in a group: will redirect you to pm, with all that chat's settings.*
-"""
-DONATE_STRING = """
-[
-🖲 I LOVE ISLAM](https://telegra.ph/I-LOVE-ISLAM-04-21)
-"""
+   - in PM: will send you your settings for all supported modules.
+   - in a group: will redirect you to pm, with all that chat's settings.
+
+{}
+And the following:
+""".format(dispatcher.bot.first_name, "" if not ALLOW_EXCL else "\nAll commands can either be used with / or !.\n")
+
+DONATE_STRING = """Heya, glad to hear you want to donate!
+It took lots of work for [my creator](t.me/SonOfLars) to get me to where I am now, and every donation helps \
+motivate him to make me even better. All the donation money will go to a better VPS to host me, and/or beer \
+(see his bio!). He's just a poor student, so every little helps!
+There are two ways of paying him; [PayPal](paypal.me/PaulSonOfLars), or [Monzo](monzo.me/paulnionvestergaardlarsen)."""
+
 IMPORTED = {}
 MIGRATEABLE = []
 HELPABLE = {}
@@ -354,6 +374,7 @@ def get_settings(bot: Bot, update: Update):
     else:
         send_settings(chat.id, user.id, True)
 
+
 @run_async
 def donate(bot: Bot, update: Update):
     user = update.effective_message.from_user
@@ -364,7 +385,7 @@ def donate(bot: Bot, update: Update):
 
         if OWNER_ID != 254318997 and DONATION_LINK:
             update.effective_message.reply_text("You can also donate to the person currently running me "
-                                                "[here]()".format(DONATION_LINK),
+                                                "[here]({})".format(DONATION_LINK),
                                                 parse_mode=ParseMode.MARKDOWN)
 
     else:
@@ -405,9 +426,7 @@ def main():
     settings_handler = CommandHandler("settings", get_settings)
     settings_callback_handler = CallbackQueryHandler(settings_button, pattern=r"stngs_")
 
-    migrate_handler = MessageHandler(Filters.status_update.migrate, migrate_chats)
-
-    khamis_handler = CommandHandler("khamis", donate)
+    donate_handler = CommandHandler("donate", donate)
     migrate_handler = MessageHandler(Filters.status_update.migrate, migrate_chats)
 
     # dispatcher.add_handler(test_handler)
@@ -417,7 +436,7 @@ def main():
     dispatcher.add_handler(help_callback_handler)
     dispatcher.add_handler(settings_callback_handler)
     dispatcher.add_handler(migrate_handler)
-    dispatcher.add_handler(khamis_handler)
+    dispatcher.add_handler(donate_handler)
 
     # dispatcher.add_error_handler(error_callback)
 
